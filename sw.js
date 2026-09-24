f49bf20e4680d0cfc713b6ef372af504e6a40b7c
Tobi Ogunleye <toubieogunleye@live.com>
2026-09-24 13:14:35 +0100

    Update to latest version 2026-09-24-v4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='tiny-routines-shell-v7';
+const CACHE='tiny-routines-shell-v9';
 const SHELL=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
